60be470e513192ea8f0cf584a501778b65f3eec8
Swift Ugandan <swiftugandan@gmail.com>
2017-03-09 21:54:34 +0000

    trying to integrate gps averaging

--- app/src/main/java/com/cytrack/assetslocation/HomeActivity.java
+++ app/src/main/java/com/cytrack/assetslocation/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.cytrack.assetslocation;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -46,29 +50,16 @@ public class HomeActivity extends AppCompatActivity {
         submitLocationBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                RequestBody requestBody = new FormBody.Builder()
-                        .add("id", assetId.getText().toString())
-                        .add("notes", assetNotes.getText().toString())
-                        .add("lat", "50.00")
-                        .add("lon", "1.00")
-                        .add("alt", "150")
-                        .add("acc", "5.0")
-                        .build();
+                try
+                {
+                    Intent intent = new Intent(Intent.ACTION_DEFAULT, Uri.parse("cz.destil.gpsaveraging.AVERAGED_LOCATION"));
+                    startActivityForResult(intent, 0);
+                } catch (ActivityNotFoundException e) {
+                    e.printStackTrace();
+                }
 
-
-                /*HttpUrl.Builder urlBuilder = HttpUrl.parse("http://demo1680352.mockable.io/").newBuilder();
-                urlBuilder.addQueryParameter("id", assetId.getText().toString());
-                urlBuilder.addQueryParameter("notes", assetNotes.getText().toString());
-                urlBuilder.addQueryParameter("lat", "50.00");
-                urlBuilder.addQueryParameter("lon", "1.00");
-                urlBuilder.addQueryParameter("alt", "150");
-                urlBuilder.addQueryParameter("acc", "5.0");
-                String url = urlBuilder.build().toString();*/
-
-                submitLocation(url, requestBody);
             }
         });
-
         client = new OkHttpClient();
     }
 
@@ -107,5 +98,25 @@ public class HomeActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = intent.getExtras();
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("id", assetId.getText().toString())
+                    .add("notes", assetNotes.getText().toString())
+                    .add("lat", String.valueOf(bundle.getDouble("latitude")))
+                    .add("lon", String.valueOf(bundle.getDouble("longitude")))
+                    .add("alt", String.valueOf(bundle.getDouble("altitude")))
+                    .add("acc", String.valueOf(bundle.getDouble("accuracy")))
+                    .build();
+            submitLocation(url, requestBody);
+        }
+        else
+        {
+            //handle cancel
+        }
     }
 }
